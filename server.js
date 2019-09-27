3c9e360fdbd13359961f745e4604edb12d2fdef1
Matt <matthew.endicott46@gmail.com>
2019-09-26 20:59:17 -0600

    server fix

--- server.js
+++ server.js
@@ -13,11 +13,15 @@ const config = require('./util/config');
 // ! Heroku Stuff
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  // app.use(express.static(path.join(__dirname, 'client/build')));
+  // ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+
 // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  // app.get('*', function(req, res) {
+  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // });
 }
 
 // - Server Config
@@ -57,6 +61,12 @@ db.once('open', () => {
 
   
 
+
+  // ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
   // - Server Connection
   app.listen(config.PORT, () =>
     console.log(`Server is connected on port ${config.PORT}`)
