4d34d4d5312d9b51b130af4fbebb7bf90a231d0f
Matt <matthew.endicott46@gmail.com>
2019-10-30 21:54:53 -0600

    initial commit

--- client/src/util/covertImage.js
+++
@@ -1,9 +0,0 @@
-// - Used to convert image buffer into an image
-
-exports.coververtImage = (profileImage, profileImageType) => {
-  if (profileImage && profileImageType) {
-    return `data:${
-      this.profileImageType
-    };charset=utf-8;base64,${this.profileImage.toString('base64')}`;
-  }
-};
